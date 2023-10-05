6426d1a0422f14636a64743814958ddb4440dc99
zzf.zhang <zhangzhanfeng@icloud.com>
2023-10-05 18:24:19 +0800

    feat: add mybatis-spring

--- src/main/java/com/github/zzf/Application.java
+++ src/main/java/com/github/zzf/Application.java
@@ -1,30 +1,57 @@
 package com.github.zzf;
 
 import com.github.zzf.dao.UserDao;
-import com.github.zzf.dao.model.User;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSession;
+import lombok.val;
+import lombok.var;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-import java.io.InputStream;
+import javax.sql.DataSource;
 
 /**
  * @author zhang.zzf
- * @date 2020-04-18
  */
 @Slf4j
+@Configuration
+@ComponentScan(basePackageClasses = {PackageInfo.class})
+@MapperScan(basePackageClasses = {PackageInfo.class})
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         log.info("Hello,World");
-        InputStream r = ClassLoader.getSystemResourceAsStream("mybatis-global-config.xml");
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(r);
-        try (SqlSession sqlSession = sessionFactory.openSession()) {
-            UserDao userDao = sqlSession.getMapper(UserDao.class);
-            User user = userDao.getBy(1L);
-            log.info("user -> {}", user);
-        }
+        val context = new AnnotationConfigApplicationContext(Application.class);
+        Thread.currentThread().join();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setUrl("jdbc:mysql://ds918:3306/test");
+        ds.setUser("root");
+        ds.setPassword("test");
+        return ds;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public UserDao userMapper() throws Exception {
+        @SuppressWarnings("resource")
+        var sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory());
+        return sqlSessionTemplate.getMapper(UserDao.class);
     }
 
 }
---
+++ src/main/java/com/github/zzf/PackageInfo.java
@@ -0,0 +1,5 @@
+package com.github.zzf;
+
+public class PackageInfo {
+
+}
